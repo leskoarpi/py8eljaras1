edef73e1d32e79d346b060d75cccb394212e1c27
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-01-19 07:18:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+1. Feladat
+Írj eljárást, amely egy tetszőleges szöveget, ill. alakzatot ír ki a képernyőre!
+'''
+def eljaras(nevszoveg, alakzat):
+    print('Szia '+ nevszoveg +', üdv a fedélzeten!')
+    print(alakzat)
+
+eljaras('nagy árpi', 'OOOOO\nO\tO\nO\tO\nO\tO\nOOOOO')
+  
